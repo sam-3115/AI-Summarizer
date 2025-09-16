7f74182bc305154a452870c092872f4772b73166
sam-3115 <123548588+sam-3115@users.noreply.github.com>
2025-09-16 23:25:45 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,15 @@
 // Simple Express backend to proxy requests to Hugging Face Inference API
+// const express = require("express");
+// const cors = require("cors");
+// const bodyParser = require("body-parser");
+// const axios = require("axios");
+
+require("dotenv").config();   // <-- add this line
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const axios = require("axios");
+
 
 const app = express();
 app.use(cors());
